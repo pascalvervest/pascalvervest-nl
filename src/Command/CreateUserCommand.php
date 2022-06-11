4c60d39ea947ff6632d3b8fa748fb1fc9e98476f
Pascal Vervest <pascalvervest@gmail.com>
2022-06-11 22:15:15 +0200

    Implement basic security and login

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(name: 'app:create-user')]
+class CreateUserCommand extends Command
+{
+    protected static $defaultName = 'app:create-user';
+    private ManagerRegistry $doctrine;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->doctrine = $doctrine;
+        $this->passwordHasher = $passwordHasher;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Create new user')
+            ->setHelp('Create new user')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'User Creator',
+            '============',
+            ''
+        ]);
+
+        $helper = $this->getHelper('question');
+
+        $emailQuestion = new Question('Enter username: ');
+        $passwordQuestion = new Question("Enter password: ");
+
+        $email = $helper->ask($input, $output, $emailQuestion);
+        $password = $helper->ask($input, $output, $passwordQuestion);
+
+        if (!$email && !$password) {
+            return Command::INVALID;
+        }
+
+        $user = new User();
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setEmail($email);
+        $user->setPassword($hashedPassword);
+        $user->setRoles(['ROLE_ADMIN']);
+
+        try {
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $output->writeln('Successfully created new user');
+        } catch (\Exception $e) {
+            $output->writeln('Unable to create user...');
+        }
+
+        return Command::SUCCESS;
+    }
+}
